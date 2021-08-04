ccc6da94fe6f3bbec0b62cfa3da70b3dbb2b5a1e
khushbakhtp <khushbakht_paracha@hotmail.com>
2021-08-04 11:53:57 -0700

    initial project

---
+++ src/test.java
@@ -0,0 +1,15 @@
+import com.sun.corba.se.impl.orb.ParserTable;
+
+import java.util.Locale;
+
+public class test {
+    public static void main(String[] args) {
+        String str="sadia";
+        str=str.ped;
+            System.out.println(str);
+        }
+
+
+    }
+
+
